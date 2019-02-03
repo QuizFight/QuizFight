689fd45cb3eede1061f8cb995b1e87b1940c3b04
bluenote9 <Oktave8?>
2019-02-03 15:26:47 +0100

    feature/QuizActivity

--- app/src/main/java/org/quizfight/quizfight/GameDetailActivity.kt
+++ app/src/main/java/org/quizfight/quizfight/GameDetailActivity.kt
@@ -1,12 +1,22 @@
 package org.quizfight.quizfight
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class GameDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_detail)
+    }
+
+    fun showQuizActivity(view: View) {
+        // Create an Intent to start the AllGamesActivity
+        val intent = Intent(this, QuizActivity::class.java)
+        // Start the new activity.
+        startActivity(intent)
+
     }
 }
