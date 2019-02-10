35621f83950b73625b01a912a289aa6ba5649a6d
philpsc <philpsc@users.noreply.github.com>
2019-02-11 00:04:40 +0100

    Change frontend network code to coroutines

--- app/src/main/java/org/quizfight/quizfight/QuizActivity.kt
+++ app/src/main/java/org/quizfight/quizfight/QuizActivity.kt
@@ -7,12 +7,17 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_quiz.*
 import org.quizfight.common.question.FourAnswersQuestion
 import android.view.View
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.quizfight.common.messages.MsgSendAnswer
+import kotlin.coroutines.CoroutineContext
 
+class QuizActivity : CoroutineScope, AppCompatActivity() {
 
-class QuizActivity : AppCompatActivity() {
+    // The code in a "launch" block should run on the main thread.
+    // Use launch{} whenever you want to change UI elements.
+    private var job = Job()
+    override val coroutineContext = Dispatchers.Main + job
+
     private var questionCounter: Int = 0
     private var questionCountTotal: Int = 0
 
@@ -26,13 +31,23 @@ class QuizActivity : AppCompatActivity() {
         //sollte auch vom Server gelesen werden
         questionCountTotal = intent.getIntExtra("questionCountTotal", 0)
 
-        Thread(Runnable {
-            client = Client("10.0.2.2", 34567, this)
-        }).start()
+        // Use launch(Dispatchers.IO){} for networking operations
+        launch(Dispatchers.IO) {
+            println("launch runs in thread ${Thread.currentThread().name}")
+            client = Client("10.0.2.2", 34567, this@QuizActivity)
+        }
 
     }
 
-    fun showNextQuestion(question: FourAnswersQuestion) {
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel() // Kills the coroutine when the activity gets destroyed
+    }
+
+    //  = launch {} tells the function to run in the main thread if not stated otherwise
+    // use = launch {} whenever the function could possibly be called from a non main thread
+    // for example all handlers!
+    fun showNextQuestion(question: FourAnswersQuestion) = launch {
        if (questionCounter < questionCountTotal) {
            currentQuestion = question
            var answerList: MutableList<String> = mutableListOf(currentQuestion.correctAnswer,
@@ -41,26 +56,26 @@ class QuizActivity : AppCompatActivity() {
                    currentQuestion.badAnswer_3)
            answerList.shuffle()
 
-           this@QuizActivity.runOnUiThread(java.lang.Runnable {
+           println("Message displayed from thread ${Thread.currentThread().name}")
+           text_view_question.text = currentQuestion.text
+           answer_button1.text = answerList[0]
+           answer_button2.text = answerList[1]
+           answer_button3.text = answerList[2]
+           answer_button4.text = answerList[3]
 
-                text_view_question.text = currentQuestion.text
-                answer_button1.text = answerList[0]
-                answer_button2.text = answerList[1]
-                answer_button3.text = answerList[2]
-                answer_button4.text = answerList[3]
+           questionCounter++
 
-                questionCounter++
+           text_view_question_count.text = ("Question: " + questionCounter
+                       + "/" + questionCountTotal)
 
 
-                text_view_question_count.text = ("Question: " + questionCounter
-                        + "/" + questionCountTotal)
-           })
        } else {
             finishQuiz()
        }
     }
 
     fun checkAnswer(view: View) {
+
         val selectedButton: Button = findViewById(radio_group.checkedRadioButtonId)
         val answer: CharSequence = selectedButton.text
         if (answer == currentQuestion.correctAnswer) {
@@ -69,7 +84,8 @@ class QuizActivity : AppCompatActivity() {
             selectedButton.setTextColor(Color.WHITE)
         }
 
-        GlobalScope.launch{
+        launch(Dispatchers.Default){
+            println("Answer sent from thread ${Thread.currentThread().name}")
             client.conn.send(MsgSendAnswer(0, 10))
         }
     }
