d48baf5cf7e38818115cf098a4cf4035492f7812
philpsc <philpsc@users.noreply.github.com>
2019-02-14 20:22:44 +0100

    Add MasterServer implementation

--- common/src/main/kotlin/org/quizfight/common/messages/Message.kt
+++ common/src/main/kotlin/org/quizfight/common/messages/Message.kt
@@ -12,11 +12,21 @@ data class GameData(val id: Int, val gameName:String, val maxPlayer: Int, val qu
 data class MsgGetAllOpenGames   (val openGames: List<GameData>) : Message
 //data class MsgGetOpenGames      (val openGames: List<GameData>) : Message
 data class MsgGetOpenGames      (val tmp: Boolean) : Message
-data class MsgJoinGame          (val id: Int, val playerName: String) : Message
 data class MsgGameJoined        (val gameData: GameData) : Message
-data class MsgCreateGame        (val gameData: GameData) :Message
 data class MsgSendGameCreated   (val gameData: GameData) :Message
 data class MsgSendQuestion      (val question: Question) : Message
 data class MsgSendAnswer        (val id: Int, val score: Int): Message
 data class MsgStartGame         (val id: Int) : Message
 data class MsgSendOpenGames     (val openGames: List<GameData>) : Message
+
+
+// Messages for Phil's implementation of master server
+// hacky shmacky, but there's nothing to send.
+// Data class needs at least constructor parameter, Unit is not serializable :(
+data class MsgRequestAllGames(val nothing : Nothing = Nothing) : Message
+
+data class MsgGameList(val gameList : List<Game>) : Message
+data class MsgJoinGame(val gameId: Int, val playerName : String) : Message
+data class MsgTransferToGameServer(val gameServer : GameServer) : Message
+data class MsgRegisterGameServer(val gameServer: GameServer) : Message
+data class MsgCreateGame(val gameName: String, val maxPlayer: Int)
--- server/src/main/kotlin/org/quizfight/server/MasterServer.kt
+++ server/src/main/kotlin/org/quizfight/server/MasterServer.kt
@@ -1,46 +1,126 @@
 package org.quizfight.server
 
+import org.quizfight.common.Connection
+
+
+
+
 /**
- * The Master Server who manages the Slave Servers.
- * First Bootstrap for all connections.
- * Inherits from GameServer
- * @author Thomas Spanier
+ * The master server that manages game servers.
+ * First contact point for all clients and game servers.
+ * @author Phillip Persch
  */
-class MasterServer(): GameServer() {
-//class MasterServer (ip: String):GameServer(ip, ip) {
-    var servers: MutableList<String> = arrayListOf()
+class MasterServer(private val port : Int) {
+    private var gameServers = mutableListOf<GameServer>()
 
-
-    /**
-     * Adds a new Slave Server to the List
-     */
-    fun addServerToList(slaveIp: String){
-        servers.add(slaveIp)
+    init{
+        acceptConnections()
     }
 
     /**
-     * Removes a Slave Server from the List
+     * Accepts all incoming connections.
      */
-    fun removeServerFromList(slaveIp: String){
-        servers.remove(slaveIp)
-    }
-
-    /**
-     * returns a List with all open Games from all Slave Servers
-     */
-    fun listAllOpenGames(): List<Game>{
-        var allGames: List<Game> = arrayListOf()
-        for(slaveServer in servers){
-            //TODO: connect to GameServer and call listOpenGames
+    private fun acceptConnections() {
+        val socket = ServerSocket(port)
+        while (!socket.isClosed) {
+            val connection = SocketConnection(socket.accept(), mapOf(
+                    MsgRegisterGameServer::class to { conn, msg -> registerGameServer(conn, msg as MsgRegisterGameServer) },
+                    MsgRequestAllGames::class to { conn, _ -> sendAllGames(conn) },
+                    MsgJoinGame::class to { conn, msg -> transferToGameServer(conn, msg as MsgJoinGame) },
+                    MsgCreateGame::class to { conn, msg -> sendLeastUsedGameServer(conn, msg as MsgCreateGame)}
+            ))
         }
-        return allGames
+    }
+
+
+    /**
+     * Checks if a gameServer already is known.
+     */
+    private fun gameServerIsKnown(gameServer: GameServer) : Boolean {
+        val knownGameServer = gameServers.find {gs -> gs.ip == gameServer.ip && gs.port == gameServer.port}
+        return knownGameServer != null
     }
 
     /**
-     * Starts the Master Server
+     * Adds a game server to gameServers.
      */
-    override fun start(){
-        //TODO: implement
-        println("Master Server started")
+    private fun addServerToList(gameServer: GameServer){
+        gameServers.add(gameServer)
+    }
+
+    /**
+     * Removes a game server from gameServers.
+     */
+    private fun removeServerFromList(gameServer: GameServer){
+        gameServers.remove(gameServer)
+    }
+    /**
+     * returns a List with all open Games from all game servers.
+     */
+    private fun listAllOpenGames(): List<Game>{
+        return gameServers.map {gameServer -> gameServer.games}.flatten()
+    }
+
+    /**
+     * Returns the game server with the lowest amount of games.
+     */
+    private fun getLeastUsedGameServer() : GameServer {
+        gameServers.sortBy { it.games.size}
+        return gameServers.first()
+    }
+
+    /**
+     * Returns the server that hosts the game with param gameId.
+     */
+    private fun getServerByGameId(gameId : Int) : GameServer? {
+        return gameServers.find{ gameServer -> gameServer.hasGameWithId(gameId)}
+    }
+
+
+
+    // HANDLERS
+
+    /**
+     * Wraps a list of all open games into a message object.
+     */
+    private fun sendAllGames(conn : Connection) {
+        val games = listAllOpenGames()
+        conn.send(MsgGameList(games))
+    }
+
+    /**
+     * Handler method for Join Game requests.
+     * Sends the server that manages the game in question.
+     */
+    private fun transferToGameServer(conn : Connection, msgJoinGame: MsgJoinGame) {
+
+        val gameServer  = getServerByGameId(msgJoinGame.gameId)
+
+        //TODO: What if == null?
+        if (gameServer != null) {
+            conn.send(MsgTransferToGameServer(gameServer))
+        }
+    }
+
+    /**
+     * Handler function for Create Game requests.
+     * Sends the server with the lowest amount of games.
+     */
+    private fun sendLeastUsedGameServer(conn : Connection, msgJoinGame: MsgCreateGame) {
+        val gameServer = getLeastUsedGameServer()
+        conn.send(MsgTransferToGameServer(gameServer))
+    }
+
+
+
+    private fun registerGameServer(conn: Connection, msg : MsgRegisterGameServer) {
+
+        val gameServer = msg.gameServer
+
+        if (gameServerIsKnown(gameServer)) {
+            removeServerFromList(gameServer)
+        }
+        addServerToList(gameServer)
     }
 }
+
