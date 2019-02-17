124417227c0a0c831f82f2d380b36bc368ce49e4
philpsc <philpsc@users.noreply.github.com>
2019-02-17 17:47:50 +0100

    Updatd message and question specification

--- common/src/main/kotlin/org/quizfight/common/question/Question.kt
+++ common/src/main/kotlin/org/quizfight/common/question/Question.kt
@@ -2,17 +2,38 @@ package org.quizfight.common.question
 
 import org.quizfight.common.messages.Message
 
-/**
- * This class represents a data-sructure to handle and organize questions.
- * Every class will have a category, a type and a correct answer.
- * @author Julian Einspenner
- * @param category is the category of a question
- * @param type is the type of a question. In QuestionUtils-class is a corresponding enumeration
- * @param correctAnswer is the only correct solution of a question
- */
-open class Question(val text: String, val category: String, val type: Type, val correctAnswer: String): Message
-
-enum class Type{
-    FOUR_ANSWERS_QUESTION,
-    RANGED_QUESTION
+enum class Category {
+    //TODO: Impelement
+    POLITICS,
+    BADGER,
+    MUSHROOM
 }
+
+interface Question<T> {
+    val text: String
+    val category: Category
+    fun evaluate(answer: T): Int
+}
+
+class ChoiceQuestion(
+        override val text: String,
+        override val category: Category,
+        val choices: List<String>,
+        val correctChoice: String
+) : Question<String> {
+    //TODO: Impelement
+    override fun evaluate(answer: String) = if (answer == correctChoice ) 100 else 0
+}
+
+class GuessQuestion(
+        override val text: String,
+        override val category: Category,
+        val range: IntRange,
+        val correctValue: Int
+) : Question<Int> {
+    override fun evaluate(answer: Int): Int {
+        //TODO: Impelement
+        return 9001
+    }
+}
+
